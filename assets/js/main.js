71881c7f30c9f1d2e5c9c2ecf3491c37dc89823d
wkevin <wkevin27@gmail.com>
2019-01-21 09:40:10 +0800

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-  // main menu toggle
-  var toggleButton = document.getElementById("menu-toggle");
-  var menu = document.getElementById("primary-nav");
-
-  if (toggleButton && menu) {
-    toggleButton.addEventListener("click", function() {
-      menu.classList.toggle("js-menu-is-open");
-    });
-  }
 
   // initialize smooth scroll
   $("a").smoothScroll({ offset: -20 });
