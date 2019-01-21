bcad0718a47ede4d002832b3e001ff2f3427c4a1
wkevin <wkevin27@gmail.com>
2019-01-21 09:42:46 +0800

    fix

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-  // initialize smooth scroll
-  $("a").smoothScroll({ offset: -20 });
-
-  // add lightbox class to all image links
-  $("a[href$='.jpg'], a[href$='.png'], a[href$='.gif']").attr("data-lity", "");
-});
